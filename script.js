37671a5eefd7684ebe2a9090f3ac790bd1fdd10a
hussam <h.almhabbis@libyana.ly>
2019-07-12 19:47:01 +0200

    if and switch statements

---
+++ script.js
@@ -0,0 +1,35 @@
+//Variables
+var x = "hussam";
+console.log(x);
+
+var a, b;
+a = 2;
+b = 4;
+ 
+console.log(a);
+
+console.log(b);
+
+// if statement
+if (a > b) {
+	 console.log(a + ' is bigger than ' + b); 
+} 
+
+else if (a === b){ 
+	console.log(a + ' is equal to ' + b); 
+}
+         
+else { 
+	console.log(a + ' is less than '+ b);
+}
+
+switch(a)
+{
+	case 1:
+		//statement1
+	case 2:
+		//statement2
+	default:
+		//default statement
+}
+ 
