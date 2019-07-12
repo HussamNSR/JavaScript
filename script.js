a605a4bf6b69d7986eff138354a40d249d4cedc2
hussam <h.almhabbis@libyana.ly>
2019-07-12 23:23:15 +0200

    Objects

--- script.js
+++ script.js
@@ -23,6 +23,8 @@ else {
 	console.log(a + ' is less than '+ b);
 }
 
+//switch statment
+
 switch(a)
 {
 	case 1:
@@ -32,4 +34,34 @@ switch(a)
 	default:
 		//default statement
 }
+
+
  
+// Arrays
+
+var cars = [1,2,4]; 
+
+
+//objects syntax 1
+
+var obj = {
+	att1: 'a',
+	att2: '2',
+	func1: function(){
+		this.att3 = this.att1 + this.att2;
+	} 
+	
+};
+obj.func1();
+console.log(obj);  
+
+		
+//objects syntax 2
+
+var obj2 =new Object();
+obj2.att1='a';
+obj2.att2='2';
+obj2.func2=function(){this.att3 = this.att1 + this.att2;}
+	
+obj2.func2();
+console.log(obj2);
